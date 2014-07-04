1da76ecfa8c15048496658e9d7a5147305359c60
bohnen <bohnen.net@gmail.com>
2014-07-04 10:28:16 +0900

    cljbreaker add game solution generator

---
+++ cljbreaker/src/cljbreaker/models/game.clj
@@ -0,0 +1,5 @@
+(ns cljbreaker.models.game)
+
+(defn create []
+  (vec (repeatedly 4 (fn [] (rand-nth ["r" "g" "b" "y"])))))
+
--- cljbreaker/src/cljbreaker/routes/home.clj
+++ cljbreaker/src/cljbreaker/routes/home.clj
@@ -1,13 +1,14 @@
 (ns cljbreaker.routes.home
   (:require [compojure.core :refer :all]
             [cljbreaker.views.layout :as layout]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [cljbreaker.models.game :as game]))
 
 (defn home []
   (when-not (session/get :game)
-    (session/put! :game (.nextInt (java.util.Random.) 1000000)))
+    (session/put! :game (apply str(game/create))))
   (layout/common
-   [:p "Welcome to clojurebreaker. Your current game id is " (session/get :game)]))
+   [:p "Welcome to clojurebreaker. Your current game solution is " (session/get :game)]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
