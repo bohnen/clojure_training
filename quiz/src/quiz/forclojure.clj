7014d707b199a863a32207f26b472050da9dddb1
bohnen <bohnen.net@gmail.com>
2014-07-07 16:02:29 +0900

    finish #134

--- quiz/src/quiz/forclojure.clj
+++ quiz/src/quiz/forclojure.clj
@@ -60,4 +60,42 @@
 ;; (fn [i] (take i '(1 1 2 3 5 8 13 21))) ;; これはひどい
 
 
+;; #38 Write a function which takes a variable number of parameters and returns the maximum value.
 
+(defn mymax [& args]
+  (first (sort > args)))
+
+;; (fn [x & xs] (reduce #(if (> %2 %1) %2 %1) x xs)) ;; lazy-seq だしこちらの方が早そう
+
+;; #28 flatten
+;; (seq? []) => false  ;; なんだってー
+;; (defn flat [coll]
+;;   reduce
+;;     (fn [l r] (if (or (seq? r) (vector? r)) (apply conj l (flat r)) (conj l r)))
+;;     [] coll)
+
+(defn myflat [coll]
+  (fn treewalk [res col]
+    (if (empty? col) res
+      (let [l (first col) r (rest col)]
+        (if (sequential? l) (treewalk (treewalk res l) r) (treewalk (conj res l) r)))))
+  [] coll)
+
+(defn treeprint [acc coll]
+  (let [l (first coll) r (rest coll)]
+    (if (sequential? l) (treeprint acc l) (println l))
+    (when-not (empty? r) (treeprint r))))
+
+;; 上記は全然ダメだった
+;; 他の回答例
+;; reduce
+;; (fn self [xs e] (concat xs (cond (coll? e) (reduce self '() e) true (list e))))
+;; '()
+
+;; (fn _flatten [items]
+;;   (if (empty? items)
+;;     '()
+;;     (concat (if (sequential? (first items))
+;;               (_flatten (first items))
+;;               (list (first items)))
+;;             (_flatten (rest items)))))
