044dd1e5c69fc38dad7d290709fc1600b9697c7d
bohnen <bohnen.net@gmail.com>
2014-09-10 23:31:56 +0900

    lein profile set

--- clojure-stg/project.clj
+++ clojure-stg/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.1"]]
+                 [quil "2.2.1"]
+                 [org.clojure/tools.namespace "0.2.6"]]
   :main ^:skip-aot clojure-stg.core
   )
